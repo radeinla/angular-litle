e0065538dbba4a8d8a8108e28df8bbae5eedba79
Reginald Eli A. Deinla <aldrineg@localhost.localdomain>
2014-03-14 11:10:11 +0800

    Wrapped function inside scope.

--- angular-litle.js
+++ angular-litle.js
@@ -14,6 +14,22 @@
 (function(window, angular, $, undefined) {
     'use strict';
 
+    /**
+     * Function to generate UUID.
+     * Taken from http://jsfiddle.net/briguy37/2MVFd/
+     * @returns {string}
+     */
+    function generateUUID() {
+        var d = new Date().getTime();
+        var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+            var r = (d + Math.random()*16)%16 | 0;
+            d = Math.floor(d/16);
+            return (c=='x' ? r : (r&0x7|0x8)).toString(16);
+        });
+        return uuid;
+    }
+
+
     // Module global settings.
     var settings = {};
 
